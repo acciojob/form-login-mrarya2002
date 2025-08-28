b4679864b010e19b35b48e9e589a8c6e46f01857
Nikhil Arya <nikhilarya3108202@gmail.com>
2025-08-28 16:27:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let fname =  document.getElementById("form1").elements[0].value;
+	let lname =  document.getElementById("form1").elements[1].value;
+	let name = fname+" "+lname;
+	alert(name);
 }
